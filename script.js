35bcf1e372e9d7ff2fdbe2c105d5b57a0d5baff2
amongos1 <Benicio.herrero@gmail.com>
2025-06-18 14:50:16 -0300

    among us

---
+++ script.js
@@ -0,0 +1,34 @@
+confirm("¿moriras para ver esta imajen?")
+alert("muchas gracias mañana la muerte va por vos")
+prompt("¿te gusta Imagine Dragons?")
+const boton = document.getElementById('minecraft')
+boton.addEventListener('click', () => {
+    window.location.href = 'https://listado.mercadolibre.com.ar/pou';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
